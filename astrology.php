51b10d68eab8e8af373e9a8f84e8e70d1c6668e7
Bhargav Raviya <46953122+bhargavraviya@users.noreply.github.com>
2021-08-22 12:29:35 +0530

    Update astrology.php

--- astrology.php
+++ astrology.php
@@ -9,7 +9,7 @@
 require_once 'src/AstroClient.php';
 
 $userId = "786";
-$apiKey = "000-000-000-786";
+$apiKey = "000-000-009-786";
 
 // make some dummy data in order to call Astro api
 $data = array(
@@ -27,13 +27,37 @@ $data = array(
 
 // instantiate AstroClient class
 $AstroData = new AstroClient($userId, $apiKey);
-$AstroData->setLanguage('hi');
+$AstroData->setLanguage('EN');
 
 // call horoscope functions of Astro Client
 
 //*****************Basic Astro****************//
+$responseData = $AstroData->getBirthDetails($data['date'], $data['month'], $data['year'], $data['hour'], $data['minute'], $data['second'], $data['latitude'], $data['longitude'], $data['timezone']);
+
+$responseData1 = $AstroData->getAstroDetails($data['date'], $data['month'], $data['year'], $data['hour'], $data['minute'], $data['second'], $data['latitude'], $data['longitude'], $data['timezone']);
 
 $responseData2 = $AstroData->getPlanetsDetails($data['date'], $data['month'], $data['year'], $data['hour'], $data['minute'], $data['second'], $data['latitude'], $data['longitude'], $data['timezone']);
+
+$responseData3 = $AstroData->getPlanetsExtendedDetails($data['date'], $data['month'], $data['year'], $data['hour'], $data['minute'], $data['second'], $data['latitude'], $data['longitude'], $data['timezone']);
+
+$responseData4 = $AstroData->getPlanetsTropicalDetails($data['date'], $data['month'], $data['year'], $data['hour'], $data['minute'], $data['second'], $data['latitude'], $data['longitude'], $data['timezone']);
+
+$responseData98 = $AstroData->getAyanamsha($data['date'], $data['month'], $data['year'], $data['hour'], $data['minute'], $data['second'], $data['latitude'], $data['longitude'], $data['timezone']);
+
+$responseData99 = $AstroData->getHouseCuspTropical($data['date'], $data['month'], $data['year'], $data['hour'], $data['minute'], $data['second'], $data['latitude'], $data['longitude'], $data['timezone']);
+
+//***************** Panchange ****************//
+$responseData91 = $AstroData->getBasicPanchang($data['date'], $data['month'], $data['year'], $data['hour'], $data['minute'], $data['second'], $data['latitude'], $data['longitude'], $data['timezone']);
+
+$responseData92 = $AstroData->getPlanetPanchang($data['date'], $data['month'], $data['year'], $data['hour'], $data['minute'], $data['second'], $data['latitude'], $data['longitude'], $data['timezone']);
+
+$responseData93 = $AstroData->getAdvancedPanchang($data['date'], $data['month'], $data['year'], $data['hour'], $data['minute'], $data['second'], $data['latitude'], $data['longitude'], $data['timezone']);
+
+//***************** Muhurat ****************//	
+
+$responseData94 = $AstroData->getChaughadiyaMuhurta($data['date'], $data['month'], $data['year'], $data['hour'], $data['minute'], $data['second'], $data['latitude'], $data['longitude'], $data['timezone']);
+
+$responseData95 = $AstroData->getHoraMuhurta($data['date'], $data['month'], $data['year'], $data['hour'], $data['minute'], $data['second'], $data['latitude'], $data['longitude'], $data['timezone']);
 
 echo  $responseData2 . PHP_EOL;
 
