b856f97f597d56fb39cc27d32c7ad78a992ff1d1
Bhargav Raviya <46953122+bhargavraviya@users.noreply.github.com>
2021-08-22 12:39:07 +0530

    Update sdk.php

--- src/sdk.php
+++ src/sdk.php
@@ -32,7 +32,7 @@ function getCurlReponse($userId, $apiKey, $resource, array $data, $language)
 
 	// Set custom headers for Auth and Content-Type header
 	// curl_setopt($curl, CURLOPT_HTTPHEADER, [
-	  // 'X-AstroAPI-Host: kvstore.p.rapidapi.com',
+	  // 'X-AstroAPI-Host: brahmsamajj.org',
 	  // 'X-AstroAPI-Key: 7xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
 	  // 'Content-Type: application/json'
 	// ]);
